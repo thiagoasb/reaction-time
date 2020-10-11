1576a730f6d8232aaa6c11b0eec48c5aae98e40b
Thiago Araújo <thiagoasbezerra@gmail.com>
2020-10-11 20:33:24 -0300

    (fix) fixing score

--- js/game.js
+++ js/game.js
@@ -84,6 +84,7 @@ start.onclick = function() {
 }
 reset.onclick = function() {
     score = 0;
+    showScore.value = score;
     document.getElementById('circle').style.display = 'none';
     console.log(score);
 }
